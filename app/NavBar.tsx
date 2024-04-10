4616136e6275f8fedcbc21a267ed67b6ee06fb28
BabarKhann <babarkhan138@gmail.com>
2024-04-10 22:16:41 +0500

    styling active menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,8 +3,12 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+import classNames from 'classnames';
+
 const NavBar = () => {
   const currentPath = usePathname();
+
+  console.log(currentPath);
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -18,7 +22,11 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            className={classNames({
+              'text-zinc-950 font-bold': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
           >
             {link.label}
           </Link>
